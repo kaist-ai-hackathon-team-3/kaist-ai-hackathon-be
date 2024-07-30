5a737a1171923580e0e5a201bd655015e1717608
dawnfire05 <dawnfire05@naver.com>
2024-07-30 10:00:11 +0900

    init profile

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ProfileModule } from './profile/profile.module';
 
 @Module({
-  imports: [AuthModule, UserModule, PrismaModule],
+  imports: [AuthModule, UserModule, PrismaModule, ProfileModule],
   controllers: [AppController],
   providers: [AppService],
 })
