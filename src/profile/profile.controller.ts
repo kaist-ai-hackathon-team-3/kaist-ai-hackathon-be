b50f9d16ca10d669c18cc1025856c914e824d4f4
kim-minkyoung <decomin02@pusan.ac.kr>
2024-07-31 16:41:22 +0900

    fix: req.user.id = 1

--- src/profile/profile.controller.ts
+++ src/profile/profile.controller.ts
@@ -27,25 +27,28 @@ import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 export class ProfileController {
   constructor(private readonly profileService: ProfileService) {}
 
-  @UseGuards(JwtAuthGuard)
+  // @UseGuards(JwtAuthGuard)
   @Post()
   @ApiOperation({ summary: '새 프로필 생성' })
   @ApiBody({ type: CreateProfileDto })
   @ApiResponse({ status: 201, description: '성공', type: CreateProfileDto })
   create(@Request() req, @Body() createProfileDto: CreateProfileDto) {
-    const data = { userId: req.user.id, ...createProfileDto };
+    // const data = { userId: req.user.id, ...createProfileDto };
+    const data = { userId: 1, ...createProfileDto };
     return this.profileService.create(data);
   }
 
-  @UseGuards(JwtAuthGuard)
+  // @UseGuards(JwtAuthGuard)
   @ApiOperation({ summary: '로그인한 유저의 모든 프로필 불러오기' })
   @ApiResponse({ status: 201, description: '성공', type: CreateProfileDto })
   @Get()
   findUserProfile(@Request() req) {
-    return this.profileService.findByUser(req.user.id);
+    const userId = 1;
+    // return this.profileService.findByUser(req.user.id);
+    return this.profileService.findByUser(userId);
   }
 
-  @UseGuards(JwtAuthGuard)
+  // @UseGuards(JwtAuthGuard)
   @Patch(':id')
   @ApiOperation({ summary: '프로필 업데이트' })
   @ApiParam({ name: 'id', required: true, description: '프로필 ID' })
@@ -56,7 +59,9 @@ export class ProfileController {
     @Body() updateProfileDto: UpdateProfileDto,
   ) {
     const profile = await this.profileService.findOne(+id);
-    if (profile.userId === req.user.id) {
+    const userId = 1;
+    // if (profile.userId === req.user.id) {
+    if (profile.userId === userId) {
       return this.profileService.update(+id, updateProfileDto);
     } else {
       throw new HttpException('권한 없음', HttpStatus.FORBIDDEN);
@@ -70,7 +75,9 @@ export class ProfileController {
   @ApiResponse({ status: 404, description: '프로필을 찾을 수 없음' })
   async remove(@Request() req, @Param('id') id: string) {
     const profile = await this.profileService.findOne(+id);
-    if (profile.userId === req.user.id) {
+    const userId = 1;
+    // if (profile.userId === req.user.id) {
+    if (profile.userId === userId) {
       return this.profileService.remove(+id);
     } else {
       throw new HttpException('권한 없음', HttpStatus.FORBIDDEN);
