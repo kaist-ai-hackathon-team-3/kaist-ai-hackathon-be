bbfcdb65bdd331abab304afd279540023ff82641
dawnfire05 <dawnfire05@naver.com>
2024-07-31 12:12:30 +0900

    fix: import configmodule to policy module

--- src/policy/policy.module.ts
+++ src/policy/policy.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PolicyService } from './policy.service';
 import { PolicyController } from './policy.controller';
 import { HttpModule } from '@nestjs/axios';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [HttpModule],
+  imports: [HttpModule, ConfigModule.forRoot()],
   controllers: [PolicyController],
   providers: [PolicyService],
 })
